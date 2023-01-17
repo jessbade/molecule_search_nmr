1d4a9d2f0a56a245d4b8e08d70c605d53d0eb81c
jessbade <jessicabade01@gmail.com>
2023-01-17 09:28:41 -0800

    Move example outside code

---
+++ workflows/example.py
@@ -0,0 +1,17 @@
+import numpy as np
+from molecule_search_nmr.search import matching_score
+
+## sample data import
+[query, pool] = np.load("../resources/sample_data.npz")["data"]  # , allow_pickle = True
+
+## molecule search
+results = {}
+for mol_id in pool.keys():
+
+    shifts = np.sort(pool[mol_id])
+    results[mol_id] = matching_score(query, shifts)
+
+
+results = dict(sorted(results.items(), reverse=True, key=lambda item: item[1])[:10])
+for i, r in enumerate(results):
+    print(i + 1, r, results[r])
